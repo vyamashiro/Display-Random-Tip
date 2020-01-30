732e8bfd330843a1a0cd05535ec451449c8db1e7
Victor Yamashiro <victor.yamashiro@gmail.com>
2020-01-30 16:54:18 -0300

    third commit

--- js/functions/checkNum.js
+++ js/functions/checkNum.js
@@ -1,13 +1,9 @@
-//Função verificar número randômico para evitar duplicidade
-
 function checkNum() {
     do {
 
         var numRandom = randomGenerator(0, (tipsList.length-1));
         
     } while (storeNum.indexOf(String(numRandom)) != -1);
-    
-    showTip(numRandom);
     
     return numRandom;
 };
